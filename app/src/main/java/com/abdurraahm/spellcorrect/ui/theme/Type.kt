4140b73f939133e67b07525f1c56f9ce4b5b84c0
Abdurr <abdurrahman.abdur02@gmail.com>
2024-08-05 15:18:28 +0700

    [ADD] New Font Typography

--- app/src/main/java/com/abdurraahm/spellcorrect/ui/theme/Type.kt
+++ app/src/main/java/com/abdurraahm/spellcorrect/ui/theme/Type.kt
@@ -2,19 +2,42 @@ package com.abdurraahm.spellcorrect.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.abdurraahm.spellcorrect.R
+
+val fontFamily = FontFamily(
+    Font(R.font.inter_28pt_light, FontWeight.Light),
+    Font(R.font.inter_28pt_regular, FontWeight.Normal),
+    Font(R.font.inter_28pt_semibold, FontWeight.SemiBold),
+    Font(R.font.inter_28pt_medium, FontWeight.Medium),
+    Font(R.font.inter_28pt_bold, FontWeight.Bold),
+
+    )
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
+    labelMedium = TextStyle(
+        fontFamily = fontFamily, fontWeight = FontWeight.Normal, fontSize = 12.sp/*...*/
+    ),
+    labelLarge = TextStyle(
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp
+//        letterSpacing = 2.sp,
+    ),
+    displayMedium = TextStyle(
+        fontFamily = fontFamily, fontWeight = FontWeight.SemiBold/*...*/
+    ),
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
