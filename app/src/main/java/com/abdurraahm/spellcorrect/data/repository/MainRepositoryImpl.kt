1ef954863f253ce3e3cdf4c9c1cfb784ab1867ac
Abdurr <abdurrahman.abdur02@gmail.com>
2024-08-07 23:23:23 +0700

    [ADD] Word Of The Day

--- app/src/main/java/com/abdurraahm/spellcorrect/data/repository/MainRepositoryImpl.kt
+++ app/src/main/java/com/abdurraahm/spellcorrect/data/repository/MainRepositoryImpl.kt
@@ -1,13 +1,20 @@
 package com.abdurraahm.spellcorrect.data.repository
 
+import android.os.Build
 import com.abdurraahm.spellcorrect.data.local.model.Section
 import com.abdurraahm.spellcorrect.data.local.model.WordEntry
 import com.abdurraahm.spellcorrect.data.local.source.NavigationDataStore
 import com.abdurraahm.spellcorrect.data.local.source.WordEntryDataSource
 import com.abdurraahm.spellcorrect.data.local.source.WordEntryDataStore
 import kotlinx.coroutines.flow.Flow
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.Date
+import java.util.Locale
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlin.random.Random
 
 @Singleton
 class MainRepositoryImpl @Inject constructor(
@@ -22,14 +29,28 @@ class MainRepositoryImpl @Inject constructor(
         navigationDataStore.updateOnboardingState(completed)
     }
 
-
     // Word Entry
     override fun wordOfTheDay(): WordEntry {
-        return wordEntryDataSource.sectionEntry(Section.FIRST)[0]
+        val wordEntry = wordEntryDataSource.mergedEntry()
+        val seed = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                // Using newer APIs for Oreo and above
+                val currentDate = LocalDate.now()
+                val formatter = DateTimeFormatter.ofPattern("ddMMyyyy")
+                "50002101" + currentDate.format(formatter)
+            } else {
+                // Fallback for older versions
+                val currentDateOlder = Date()
+                val formatterOlder = SimpleDateFormat("ddMMyyyy", Locale.getDefault())
+                "50002101" + formatterOlder.format(currentDateOlder)
+            }
+        val randomSeeded = Random(seed.toLong())
+        val wordOfTheDayIndex = wordEntry.indices.random(randomSeeded)
+        val wordOfTheDay = wordEntry[wordOfTheDayIndex]
+        return wordOfTheDay
     }
 
     override fun exerciseStart(section: Section): List<WordEntry> {
-        TODO("Not yet implemented")
+        return wordEntryDataSource.sectionEntry(section)
     }
 
     override fun exerciseResume(section: Section): List<WordEntry> {
