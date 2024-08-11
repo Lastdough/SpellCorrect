397be9141e8f710456b8d1a5e275025230e1ccda
Abdurr <abdurrahman.abdur02@gmail.com>
2024-08-11 10:37:20 +0700

    [UPDATE] FlashScreen UI, and how to handle lastIndex Pointer

--- app/src/main/java/com/abdurraahm/spellcorrect/ui/screen/flashcard/FlashScreen.kt
+++ app/src/main/java/com/abdurraahm/spellcorrect/ui/screen/flashcard/FlashScreen.kt
@@ -1,21 +1,33 @@
 package com.abdurraahm.spellcorrect.ui.screen.flashcard
 
 import android.content.res.Configuration
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ArrowBack
+import androidx.compose.material.icons.automirrored.outlined.VolumeUp
+import androidx.compose.material.icons.outlined.SkipNext
+import androidx.compose.material.icons.outlined.SkipPrevious
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
 import com.abdurraahm.spellcorrect.data.local.model.Exercise
 import com.abdurraahm.spellcorrect.data.local.model.Section
 import com.abdurraahm.spellcorrect.data.local.model.WordEntry
@@ -32,26 +44,53 @@ fun FlashScreen(
     flashViewModel: FlashViewModel = hiltViewModel(),
     exerciseState: Int
 ) {
+    val section = Section.entries[sectionId]
+
     LaunchedEffect(Unit) {
+        flashViewModel.init(section)
         when (Exercise.entries[exerciseState]) {
-            Exercise.START -> flashViewModel.startExercise(Section.entries[sectionId])
-            Exercise.RESUME -> flashViewModel.resumeExercise(Section.entries[sectionId])
+            Exercise.START -> flashViewModel.startExercise(section)
+            Exercise.RESUME -> flashViewModel.resumeExercise(section)
         }
     }
     val context = LocalContext.current
-    when(val shuffledWords = flashViewModel.shuffledWords.collectAsState().value){
+    when (val shuffledWords = flashViewModel.shuffledWords.collectAsState().value) {
         is UiState.Error -> {
         }
+
         UiState.Loading -> {
             CircularLoading()
         }
+
         is UiState.Success -> {
+            val list = shuffledWords.data
+            val index = flashViewModel.lastIndex.intValue
+            val word = list[index]
             FlashContent(
                 modifier = modifier,
-                navController = navController,
-                list = shuffledWords.data,
-                i = sectionId
+                word = word,
+                onWordClicked = {
+                    flashViewModel.speak(word.word)
+                },
+                onPreviousClicked = {
+                    flashViewModel.previousLastIndexed(section)
+                },
+                onNextClicked = {
+                    flashViewModel.nextLastIndexed(section)
+                },
+                onBackButtonClicked = {
+
+                },
+                onDefinitionClicked = {
+                    flashViewModel.speak(word.fullDescription)
+                },
+                currentIndex = index,
+                lastIndex = list.lastIndex,
             )
+            BackHandler {
+                navController.navigateUp()
+                flashViewModel.endExercise(section = section)
+            }
         }
     }
 
@@ -61,20 +100,71 @@ fun FlashScreen(
 @Composable
 private fun FlashContent(
     modifier: Modifier = Modifier,
-    navController: NavHostController,
-    i: Int,
-    list: List<WordEntry>
+    word: WordEntry,
+    onWordClicked: () -> Unit,
+    onPreviousClicked: () -> Unit,
+    onNextClicked: () -> Unit,
+    onBackButtonClicked: () -> Unit,
+    currentIndex: Int,
+    lastIndex: Int,
+    onDefinitionClicked: () -> Unit
 ) {
     Scaffold(modifier = modifier) {
         // Content
         Column(
             modifier = Modifier
                 .padding(it)
-                .padding(horizontal = 16.dp, vertical = 8.dp)
+                .padding(horizontal = 16.dp, vertical = 32.dp)
                 .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            Text(text = "Flash Card $i")
-            Text(text = list.take(1).toString())
+            Column() {
+                Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                    Text(text = word.wordFirstLetterCapitalized)
+                    Text(text = word.typeClean)
+                }
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    if (word.hasIPA) {
+                        Text(text = word.ipa)
+                    }
+                    IconButton(onClick = onWordClicked) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Outlined.VolumeUp,
+                            contentDescription = null
+                        )
+                    }
+                }
+            }
+            Text(modifier = Modifier.clickable {
+                onDefinitionClicked()
+            }, text = word.definitionWithNumber, textAlign = TextAlign.Justify)
+            Row(
+                modifier = Modifier.fillMaxSize(),
+                Arrangement.SpaceBetween
+            ) {
+                IconButton(onClick = onBackButtonClicked) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Outlined.ArrowBack,
+                        contentDescription = null
+                    )
+                }
+                Row(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+                    IconButton(onClick = onPreviousClicked, enabled = currentIndex > 0) {
+                        // Disable at first index
+                        Icon(
+                            imageVector = Icons.Outlined.SkipPrevious,
+                            contentDescription = null
+                        )
+                    }
+                    IconButton(onClick = onNextClicked, enabled = currentIndex < lastIndex) {
+                        // Disable at last index
+                        Icon(
+                            imageVector = Icons.Outlined.SkipNext,
+                            contentDescription = null
+                        )
+                    }
+                }
+            }
         }
     }
 }
@@ -85,8 +175,14 @@ private fun FlashContent(
 private fun FlashContentPreview() {
     SpellCorrectTheme {
         FlashContent(
-            navController = rememberNavController(), i = 0,
-            list = listOf(PreviewDataSource.singleWord())
+            word = PreviewDataSource.singleWord(),
+            onWordClicked = {},
+            onPreviousClicked = {},
+            onNextClicked = {},
+            onDefinitionClicked = {},
+            onBackButtonClicked = {},
+            currentIndex = 0,
+            lastIndex = 0,
         )
     }
 }
--- app/src/main/java/com/abdurraahm/spellcorrect/ui/screen/flashcard/FlashViewModel.kt
+++ app/src/main/java/com/abdurraahm/spellcorrect/ui/screen/flashcard/FlashViewModel.kt
@@ -1,14 +1,19 @@
 package com.abdurraahm.spellcorrect.ui.screen.flashcard
 
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.abdurraahm.spellcorrect.data.local.model.Section
+import com.abdurraahm.spellcorrect.data.local.model.SectionData
 import com.abdurraahm.spellcorrect.data.local.model.WordEntry
 import com.abdurraahm.spellcorrect.data.repository.MainRepository
 import com.abdurraahm.spellcorrect.ui.state.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -16,14 +21,31 @@ import javax.inject.Inject
 class FlashViewModel @Inject constructor(
     private val mainRepository: MainRepository,
 ) : ViewModel() {
+    fun setRate(newRate: Float) = mainRepository.updateRate(newRate)
+    fun speak(text: String) = viewModelScope.launch(Dispatchers.IO) {
+        mainRepository.speak(text)
+    }
 
     private val _shuffledWords: MutableStateFlow<UiState<List<WordEntry>>> =
         MutableStateFlow(UiState.Loading)
     val shuffledWords
         get() = _shuffledWords.asStateFlow()
 
+
+    private val _lastIndex = mutableIntStateOf(0) // Initial value will be overwritten
+    val lastIndex
+        get() = _lastIndex
+
+    fun init(section: Section) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val initialIndex = mainRepository.getLastIndexed(section).first()
+            _lastIndex.intValue = initialIndex
+        }
+    }
+
+
     fun startExercise(section: Section) {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             mainRepository.exerciseStart(section = section).collect { list ->
                 _shuffledWords.value = UiState.Success(list)
             }
@@ -31,10 +53,47 @@ class FlashViewModel @Inject constructor(
     }
 
     fun resumeExercise(section: Section) {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             mainRepository.exerciseResume(section = section).collect { list ->
                 _shuffledWords.value = UiState.Success(list)
             }
         }
     }
+
+    fun nextLastIndexed(section: Section) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val listSize = mainRepository.getSectionListSize(section).first()
+            mainRepository.getLastIndexed(section).first().let { currentIndex ->
+                val nextIndex = (currentIndex + 1).coerceAtMost(listSize - 1) // Bound check
+                mainRepository.saveLastIndexed(index = nextIndex, section)
+                _lastIndex.intValue = nextIndex
+            }
+        }
+    }
+
+    fun previousLastIndexed(section: Section) {
+        viewModelScope.launch(Dispatchers.IO) {
+            mainRepository.getLastIndexed(section).first().let { currentIndex ->
+                val prevIndex = (currentIndex - 1).coerceAtLeast(0) // Boundcheck
+                mainRepository.saveLastIndexed(index = prevIndex, section)
+                _lastIndex.intValue = prevIndex
+            }
+        }
+    }
+
+    fun endExercise(section: Section) {
+        viewModelScope.launch(Dispatchers.IO) {
+            mainRepository.getLastIndexed(section).collect { i ->
+                mainRepository.exerciseEnd(section, i)
+            }
+        }
+    }
+
+    fun updateSectionData(sectionData: SectionData) {
+        viewModelScope.launch(Dispatchers.IO) {
+            mainRepository.updateSectionData(sectionData)
+        }
+    }
 }
+
+
